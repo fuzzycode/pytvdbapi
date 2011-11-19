80a91868984c4c157f4565d3c84f98a6700f8e63
Bjorn Larsson <b.e.larsson@gmail.com>
2011-11-19 19:41:15 +0100

    Added api test file.

---
+++ thetvdb/tests/test_api.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+# Copyright 2011 Björn Larsson
+
+# This file is part of thetvdb.
+#
+# thetvdb is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# thetvdb is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with thetvdb.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import unittest
+
+from thetvdb.tests import basetest
+
+
+class TestApi(basetest.TheTVDBTest):
+    pass
+
+
+if __name__ == "__main__":
+    sys.exit(unittest.main())
