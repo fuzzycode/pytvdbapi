b3be181de15c3166a348776c64d0af40af1a9e05
Bjorn Larsson <b.e.larsson@gmail.com>
2012-06-29 15:34:16 +0200

    Updated to version 0.3

--- pytvdbapi/__init__.py
+++ pytvdbapi/__init__.py
@@ -29,7 +29,7 @@ try:
 except ImportError:
     from pytvdbapi.backport import NullHandler
 
-__VERSION__ = (0, 2, 3)
+__VERSION__ = (0, 3, 0)
 __NAME__ = 'pytvdbapi'
 __EMAIL__ = 'develop@bjornlarsson.net'
 
