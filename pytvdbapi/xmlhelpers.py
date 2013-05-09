f132d67b8f29f980f9920729fa3c8e93134aaa22
Björn Larsson <b.e.larsson@gmail.com>
2013-05-09 20:39:13 +0200

    Removed pylint command.

--- pytvdbapi/xmlhelpers.py
+++ pytvdbapi/xmlhelpers.py
@@ -17,8 +17,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with pytvdbapi.  If not, see <http://www.gnu.org/licenses/>.
 
-# pylint: disable=R0922
-
 """
 A helper module for parsing a XML data.
 """
