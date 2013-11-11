9444188d620ba612af73acb89d62fb04c3a34951
Björn Larsson <b.e.larsson@gmail.com>
2013-11-11 22:03:58 +0100

    Added readthedocs configuration.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,8 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os
+import sys
+import os
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -21,6 +22,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(__file__, "../../../")))
 from pytvdbapi.__init__ import __NAME__, version
 
 
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+
 # -- General configuration -----------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -28,7 +32,8 @@ from pytvdbapi.__init__ import __NAME__, version
 
 # Add any Sphinx extension module names here, as strings. They can be extensions
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo', 'sphinx.ext.coverage', 'sphinx.ext.ifconfig']
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
+              'sphinx.ext.coverage', 'sphinx.ext.ifconfig']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -94,7 +99,10 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+if on_rtd:
+    html_theme = 'nature'
+else:
+    html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -173,21 +181,21 @@ htmlhelp_basename = 'pytvdbapidoc'
 # -- Options for LaTeX output --------------------------------------------------
 
 latex_elements = {
-# The paper size ('letterpaper' or 'a4paper').
-#'papersize': 'letterpaper',
+    # The paper size ('letterpaper' or 'a4paper').
+    #'papersize': 'letterpaper',
 
-# The font size ('10pt', '11pt' or '12pt').
-#'pointsize': '10pt',
+    # The font size ('10pt', '11pt' or '12pt').
+    #'pointsize': '10pt',
 
-# Additional stuff for the LaTeX preamble.
-#'preamble': '',
+    # Additional stuff for the LaTeX preamble.
+    #'preamble': '',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'pytvdbapi.tex', u'pytvdbapi Documentation',
-   u'Bjorn Larsson', 'manual'),
+    ('index', 'pytvdbapi.tex', u'pytvdbapi Documentation',
+     u'Björn Larsson', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -196,7 +204,7 @@ latex_documents = [
 
 # For "manual" documents, if this is true, then toplevel headings are parts,
 # not chapters.
-#latex_use_parts = False
+latex_use_parts = True
 
 # If true, show page references after internal links.
 #latex_show_pagerefs = False
@@ -217,7 +225,7 @@ latex_documents = [
 # (source start file, name, description, authors, manual section).
 man_pages = [
     ('index', 'pytvdbapi', u'pytvdbapi Documentation',
-     [u'Bjorn Larsson'], 1)
+     [u'Björn Larsson'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -230,9 +238,9 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'pytvdbapi', u'pytvdbapi Documentation',
-   u'Bjorn Larsson', 'pytvdbapi', 'One line description of project.',
-   'Miscellaneous'),
+    ('index', 'pytvdbapi', u'pytvdbapi Documentation',
+     u'Björn Larsson', 'pytvdbapi', 'One line description of project.',
+     'Miscellaneous'),
 ]
 
 # Documents to append as an appendix to all manuals.
